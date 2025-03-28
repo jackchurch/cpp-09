6da2eee9ba077228e48c163b4bc310b542224770
Jack Church <hello@jack.church>
2025-03-29 00:29:47 +1030

    Ex01 works.

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -11,13 +11,13 @@ RPN::RPN(const RPN & orignal)
 RPN &RPN::operator=(const RPN& orignal)
 {
     this->_stack = orignal._stack;
-    return *this;
+    return (*this);
 }
 
 bool RPN::isOperator(std::string s)
 {
-    if (s.compare("+") || s.compare("-") 
-    || s.compare("*") || s.compare("/"))
+    if (s.compare("+") == 0 || s.compare("-") == 0 
+    || s.compare("*") == 0 || s.compare("/") == 0)
     {
         return (true);
     }
@@ -29,70 +29,54 @@ bool    RPN::doCalculation(std::string s)
     double     left;
     double     right;
 
-    // Try to convert stack char in to double
-    try
-    {
-        std::istringstream (s) >> right;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << "Error: " << s << " is not a number." << std::endl;;
-    }
-    
+    right = this->_stack.top();
+    this->_stack.pop();
+    left = this->_stack.top();
     this->_stack.pop();
 
-    // Try to convert stack char in to double
-    try
+    switch (s[0])
     {
-        std::istringstream (s) >> left;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << "Error: " << s << " is not a number." << std::endl;;
-    }
-
-    this->_stack.pop();
-
-    // What is the operator
-    if (s.compare("+"))
-    {
-        this->_stack.push(left + right);
-    }
-    else if (s.compare("-"))
-    {
-        this->_stack.push(left - right);
-    }
-    else if (s.compare("*"))
-    {
-        this->_stack.push(left * right);
-    }
-    else
-    {
-        if (right == 0)
+        case '+':
         {
-            std::cerr << "Error: cannot divide by 0" << std::endl;
-            return (false);   
+            this->_stack.push(left + right);
+            break ;
         }
-        this->_stack.push(left / right);
+        case '-':
+        {
+            this->_stack.push(left - right);
+            break ;
+        }
+        case '*':
+        {
+            this->_stack.push(left * right);
+            break ;
+        }
+        case '/':
+        {
+            if (right == 0)
+            {
+                std::cerr << "Error: cannot divide by 0" << std::endl;
+                exit (1);   
+            }
+            this->_stack.push(left / right);
+            break ;
+        }
     }
     return (true);
 }
 
-
 int RPN::calculate(int argc, char* argv[])
 {
-    if (argc <= 3)
+    if (argc != 2)
     {
         std::cerr << "Error" << std::endl;
         return (1);
     }
 
-    std::stringstream ss(argv[1]);
+    std::istringstream ss(argv[1]);
     std::string token;
-    int i = 0;
     while (ss >> token)
     {
-        std::cout << i << std::endl;
         if (token.length() != 1)
         {
             std::cerr << "Error" << std::endl;
@@ -110,16 +94,13 @@ int RPN::calculate(int argc, char* argv[])
                 std::cerr << "Error: unknown calculation error." << std::endl;
                 return (1);
             }
+            continue ;
         }
-        try
+        if (std::isdigit(token[0]))
         {
             this->_stack.push(std::atoi(token.c_str()));
-            std::cout << "SGACK" << this->_stack.top() << std::endl;
-        }
-        catch(const std::exception& e)
-        {
-            std::cerr << e.what() << "Error: string to interger." << std::endl;
         }
     }
+    std::cout << this->_stack.top() << std::endl;
     return (0);
 }
